43fbf609592cff1208ead13d9f58eb29fd5350ef
Dwayne Macgowan <dwaynemac@gmail.com>
2010-09-21 08:39:07 -0300

    currency dropdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def select_currency(form)
+    form.input :currency, :as => :select, :collection => %W( ars brl usd eur )
+  end
 end
